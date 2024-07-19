3db9aaa299b75954184ad5adb6def16cea5e34bc
Leandro Vedia <leandrovedia@gmail.com>
2024-07-19 14:15:36 -0300

    ARREGLO 3

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ function generateQRCode() {
         alert('Por favor, ingresa una URL.');
         return;
     }
-    
-    // Incrementar el tamaño del QR a 350
+
     const qr = new QRious({
         value: url,
         size: 350
@@ -19,7 +18,7 @@ function generateQRCode() {
 
     const logo = new Image();
     logo.onload = () => {
-        const logoSize = 70; // Aumentar el tamaño del logo
+        const logoSize = 70;
         context.drawImage(logo, (canvas.width - logoSize) / 2, (canvas.height - logoSize) / 2, logoSize, logoSize);
 
         const img = new Image();
@@ -27,13 +26,48 @@ function generateQRCode() {
         const qrcodeContainer = document.getElementById('qrcode');
         qrcodeContainer.innerHTML = '';
         qrcodeContainer.appendChild(img);
-        
+
         const urlDisplay = document.getElementById('url-display');
         urlDisplay.textContent = `URL: ${url}`;
-        
-        // Mostrar las instrucciones
+
         const instructionsContainer = document.getElementById('instructions-container');
         instructionsContainer.style.display = 'block';
     };
-    logo.src = 'logo1.png'; // Asegúrate de tener tu logo en la misma carpeta o especificar la ruta correcta
+    logo.src = 'logo1.png';
 }
+
+document.getElementById('downloadPdf').addEventListener('click', function () {
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF();
+
+    const instructions = "El siguiente Codigo QR brinda acceso al mapa interactivo del despliegue de la presente Orden de Servicio, para ello se deberá escanear mediante camara correspondiente a dispositivo movil para visualizar su contenido.";
+    const qrCanvas = document.getElementById('qrcode').querySelector('img');
+    const url = document.getElementById('url-display').innerText;
+
+    const pageWidth = doc.internal.pageSize.getWidth();
+    const margin = 20;
+    const maxWidth = pageWidth - margin * 2;
+
+    // Ajustar y centrar el texto manualmente
+    const splitInstructions = doc.splitTextToSize(instructions, maxWidth);
+    const textHeight = doc.getTextDimensions(splitInstructions).h;
+
+    doc.setFontSize(12);
+    const textY = margin + textHeight;
+    doc.text(splitInstructions, pageWidth / 2, margin, { align: "center" });
+
+    // Añadir la imagen QR centrada
+    const qrDataUrl = qrCanvas.src;
+    const qrImageWidth = 70;
+    const qrImageHeight = 70;
+    const qrImageX = (pageWidth - qrImageWidth) / 2;
+    const qrImageY = margin + textHeight + 10;
+    doc.addImage(qrDataUrl, 'PNG', qrImageX, qrImageY, qrImageWidth, qrImageHeight);
+
+    // Añadir la URL centrada
+    doc.setFontSize(12);
+    const urlY = qrImageY + qrImageHeight + 20;
+    doc.text(url, pageWidth / 2, urlY, { align: "center" });
+
+    doc.save('QR_Code_Document.pdf');
+});
